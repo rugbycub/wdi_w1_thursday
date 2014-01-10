6e89b3c12bae4a6d0bd68ac763cfadf76436c312
Charlie Nuttall <CENuttall@mac.com>
2014-01-10 02:30:30 -0800

    added rescue for error

--- gluten_free.rb
+++ gluten_free.rb
@@ -16,24 +16,38 @@ require "pry"
 # Create a Person class. A person will have a stomach and allergies
 # Create a method that allows the person to eat and add arrays of food to their stomachs
 # If a food array contains a known allergy reject the food.
+
+
+#create allergy error
 class AllergyError < ArgumentError; end
 
+#create person class
 class Person
 
+	#initialize person class
 	def initialize(allergies = nil, stomach=[])
 		@stomach = stomach
 		@allergies = allergies
 	end
 
+	#define eat and pass in food array
 	def eat(food)
-		food.each {|food| @stomach.push(food)}
-		if @stomach.include? @allergies
-				@stomach.each {|x| @stomach.pop()}
-				raise AllergyError.new("This person is allergic to #{@allergies}")
-		end
-		puts @stomach
-	end
 
+		#add food array to somach contents
+		food.each {|food| @stomach.push(food)}
+
+		#check if any ingredient matches the allergies
+		if @stomach.include? @allergies
+
+				#if yes, empty contents of stomach array and raise allergy error
+				@stomach.each {|x| @stomach.pop()}
+				begin
+					raise AllergyError.new("#{@allergies}")
+				rescue AllergyError => e
+					puts "there was an allergic reaction to #{e}"
+				end
+		end
+	end
 end
  
 # Create a Person named Chris. Chris is allergic to gluten.
@@ -45,10 +59,11 @@ water = ["h", "h", "o"]
 
 # When a person attempts to eat a food they are allergic to, raise a custom exception
 # For example:  AllergyError
- chris = Person.new("gluten")
+chris = Person.new("gluten")
+beth = Person.new("scallops")
 
  chris.eat(pizza)
- 
+ beth.eat(pan_seared_scallops)
 
 # Bonus: When a person attempts to eat a food they are allergic to,
 #        ... remove ALL the food from the person's stomach before raising the exception
